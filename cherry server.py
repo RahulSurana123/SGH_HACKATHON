3a2f5cd4e4d61f0890b3de395fedc156cb496613
unknown <rasuru04@gmail.com>
2020-02-27 16:05:54 +0530

    api from diff computer done

--- cherry server.py
+++ cherry server.py
@@ -1,8 +1,20 @@
 import cherrypy
+import request
 class full(object):
-    hi=4
+    @cherrypy.expose
+    def __index__(self):
+        self.hi = 5
+        print("highdfgknkldnfvls")
+        return
+    # def yo(self) :
+    @cherrypy.expose
     def yo(self):
-        self.hi=5
-        return self.hi
+        self.hi = 5
+
+        print("highdfgknkldnfvls")
+        return {"rahul":"ok"}
+
+
 cherrypy.server.socket_host = '0.0.0.0' # put it here
+cherrypy.config.update({'server.socket_port':8080,})
 cherrypy.quickstart(full())
