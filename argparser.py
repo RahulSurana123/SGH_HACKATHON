1a366fa39d6f2195c9f0930514a1be13ccf8de2f
RahulSurana123 <rasuru04@gmail.com>
2020-02-28 00:26:45 +0530

    argparse some cherrypy_issue

--- argparser.py
+++ argparser.py
@@ -1,38 +1,69 @@
-import argparse
-import serial
-import time
-def check(x):
-    global prev_state,duration
-    for i in range(2):
-        if(x[i]==0 and prev_state==1):
-            timer[i]=time.time()
-            print(timer[i])
-        elif(x[i]==1 and prev_state==0):
-            duration[i]=timer[i]-time.time()
-            print(duration[i]+" " +i)
-            
-            
+import serial,argparse
+import requests
+#def check(x,y):
+#    global prev_state,duration,cost,pay
+    
+#    for i in range(2):
+#        if(x[i]==0 and y[i]==1):
+#            timer[i]=time.time()
+#            print(timer[i])
+#        elif(x[i]==1 and y[i]==0):
+#            duration[i]=timer[i]-time.time()
+#            print(duration[i]+" "+i)
+#        else:
+#            print("x:  "+str(x))
+#            print("y :  "+str(y))
+#            pay[i]=duratin[i]*cost
+#            print("amount"+pay[i])
+#def time_start():
+#    return time.time()
+#def pre_state_update():
+#    prev_state=parking_valid
+#        
 parser = argparse.ArgumentParser(description="com port")
+  # add expected arguments
 parser.add_argument('--com', dest='com', required=True)
-comp = parser.parse_args()
-ser=comp.com
+
+  # parse args
+comp= parser.parse_args()
+    
+ser = comp.com
 serl = serial.Serial(ser,9600)
-duration=[0,0,0]
-timer=[0,0,0]
-prev_state=[0,0,0]
+#pay=[0,0,0]
+#cost=10
+#duration=[0,0,0]
+#timer=[0,0,0]
+prev_state=[1,1,1]
+parking_valid=[1,1,1]
+st_url="http://192.168.43.250:8080/update?p="
+#update=True
 while True:
+   
+
+    line = str(serl.readline())
+#        i=0
+    print(parking_valid)
     
-    line = serl.readline()
-#    line1=int(line)
     parking_valid=[int(i) for i in line.split(",")]
-#    print(line)
-    check(parking_valid);
-#    global prev_state
-    prev_state=parking_valid
+    if prev_state != parking_valid:
+        st_url=st_url+str(parking_valid[0])+"&q="+str(parking_valid[1])+"&r="+str(parking_valid[2])
+        requests.get(url=st_url)
+        prev_state=parking_valid
     
-#    if parking_valid[0]==0:
-#        timer[0]=time.time()
+#    prev_state=parking_valid
+#    for i in range(2):
+#        if parking_valid[i] == 0 and update==False:
+#            timer[i]=time.time()
+#            print(timer[i])        
+#            update =True
+#        elif parking_valid[i]== 1 and update== True:
+#            duration[i]=time.time()-timer[i]
 #            
-#    else:
-#        print("kuch bhi")
-#            
+#            update= False
+            
+        
+#    check(parking_valid,prev_state)
+#    time.sleep(2)
+    
+    
+    
