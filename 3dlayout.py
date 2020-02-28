27152cca011c2e34a99aa12b903e80050960074b
emu3666 <pravi7031@gmail.com>
2020-02-28 12:56:39 +0530

    3d updated version

--- 3dlayout.py
+++ 3dlayout.py
@@ -9,10 +9,10 @@ class layout(object):
     # def __init__(self):
     #     global ax,plt
         
-    @cherrypy.expose
-    def update(self,p,q,r):
+    @cherrypy.expose()
+    def update(self,p):
         global ax,plt
-        fig= plt.figure()
+        plt.figure()
         ax=plt.axes(projection="3d")
         # n=2
         m=4
@@ -34,51 +34,100 @@ class layout(object):
                 xline= x+np.zeros(len(yline))
                 zline=z+np.zeros(len(yline))
                 ax.plot3D(xline,yline,zline,"gray")
-        if(p == '1' and q=='1' and r=='1'):
-            self.red(0.5,0.5,0.5)
-            self.red(0.5,0.5,1.5)
-            self.red(0.5,2.5,2.5)
-        elif(p == '1' and q=='0' and r=='1'):
-            self.red(0.5,0.5,0.5)
-            self.green(0.5,0.5,1.5)
-            self.red(0.5,2.5,2.5)
-        elif(p == '1' and q=='1' and r=='0'):
-            self.red(0.5,0.5,0.5)
-            self.red(0.5,0.5,1.5)
-            self.green(0.5,2.5,2.5)
-            # plt.show()
-        elif(p == '0' and q=='1' and r=='1'):
+        if(p == '0'):
             self.green(0.5,0.5,0.5)
-            self.red(0.5,0.5,1.5)
-            self.red(0.5,2.5,2.5)
+        elif(p == '1'):
+            self.green(0.5,1.5,0.5)
+        elif(p == '2'):
+            self.green(0.5,2.5,0.5)
+            # plt.show()
+        elif(p == '3'):
+            self.green(1.5,0.5,0.5)
             # plt.show()    
-        elif(p == '1' and q=='0' and r=='0'):
-            self.red(0.5,0.5,0.5)
-            self.green(0.5,0.5,1.5)
-            self.green(0.5,2.5,2.5)
+        elif(p == '4'):
+            self.green(1.5,1.5,0.5)
             # plt.show()
-        elif(p == '0' and q=='1' and r=='0'):
-            self.green(0.5,0.5,0.5)
-            self.red(0.5,0.5,1.5)
-            self.green(0.5,2.5,2.5)
+        elif(p == '5'):
+            self.green(1.5,2.5,0.5)
+        elif(p == '6'):
+            self.green(2.5,0.5,0.5)
+        elif(p == '7'):
+            self.green(2.5,1.5,0.5)
             # plt.show()
-        elif(p == '0' and q=='0' and r=='1'):
+        elif(p == '8'):
+            self.green(2.5,2.5,0.5)
+            # plt.show()    
+        elif(p == '9'):
             self.green(0.5,0.5,0.5)
+        elif(p == '10'):
             self.green(0.5,0.5,1.5)
-            self.red(0.5,2.5,2.5)
+        elif(p == '11'):
+            self.green(0.5,1.5,1.5)
+            # plt.show()
+        elif(p == '12'):
+            self.green(1.5,0.5,1.5)
+            # plt.show()    
+        elif(p == '13'):
+            self.green(1.5,1.5,1.5)
+            # plt.show()
+        elif(p=='14'):
+            self.green(1.5,2.5,1.5)
+        elif(p == '15'):
+            #self.red(0.5,0.5,0.5)
+            self.green(2.5,0.5,1.5)
+            #self.red(0.5,2.5,2.5)
+        elif(p == '16'):
+            #self.red(0.5,0.5,0.5)
+            #elf.red(0.5,0.5,1.5)
+            self.green(2.5,1.5,1.5)
+            # plt.show()
+        elif(p == '17'):
+            self.green(2.5,2.5,1.5)
+            #self.red(0.5,0.5,1.5)
+            #self.red(0.5,2.5,2.5)
+            # plt.show()    
+        elif(p == '18'):
+            self.green(0.5,0.5,2.5)
+            # plt.show()
+        elif(p == '19'):
+            self.green(0.5,1.5,2.5)
+        elif(p == '20'):
+            #self.red(0.5,0.5,0.5)
+            self.green(0.5,2.5,2.5)
+            #self.red(0.5,2.5,2.5)
+        elif(p == '21'):
+            #self.red(0.5,0.5,0.5)
+            #self.red(0.5,0.5,1.5)
+            self.green(1.5,0.5,2.5)
+            # plt.show()
+        elif(p == '22'):
+            self.green(1.5,1.5,2.5)
+            #self.red(0.5,0.5,1.5)
+            #self.red(0.5,2.5,2.5)
+            # plt.show()    
+        elif(p == '23'):
+            self.green(1.5,2.5,2.5)
+        elif(p == '24'):
+            #self.red(0.5,0.5,0.5)
+            self.green(2.5,0.5,2.5)
+            #self.red(0.5,2.5,2.5)
+        elif(p == '25'):
+            #self.red(0.5,0.5,0.5)
+            #self.red(0.5,0.5,1.5)
+            self.green(2.5,1.5,2.5)
             # plt.show()
         else:
-            self.green(0.5,0.5,0.5)
-            self.green(0.5,0.5,1.5)
-            self.green(0.5,2.5,2.5)
-            # plt.show()
+            self.green(2.5,2.5,2.5)
+           # self.red(0.5,0.5,1.5)
+            #self.red(0.5,2.5,2.5)
+            # plt.show()    
         plt.show()
         return {"updated":"ok"}
 
 
-    def red(self,x,y,z):
-        global ax
-        ax.scatter([x], [y], [z], color="r", s=1000)
+    # def red(self,x,y,z):
+    #     global ax
+    #     ax.scatter([x], [y], [z], color="r", s=1000)
 
     def green(self,x,y,z):
         global ax
