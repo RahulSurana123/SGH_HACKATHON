786c74eaf45cd8ac4d7aaac5771955372a64560b
RahulSurana123 <rasuru04@gmail.com>
2020-02-28 05:42:15 +0530

    errorr donot use again

--- argparser.py
+++ argparser.py
@@ -1,5 +1,7 @@
 import serial,argparse
 import requests
+import threading
+import time
 #def check(x,y):
 #    global prev_state,duration,cost,pay
     
@@ -20,36 +22,66 @@ import requests
 #def pre_state_update():
 #    prev_state=parking_valid
 #        
-parser = argparse.ArgumentParser(description="com port")
-  # add expected arguments
-parser.add_argument('--com', dest='com', required=True)
 
-  # parse args
-comp= parser.parse_args()
+
+def argu():
     
-ser = comp.com
-serl = serial.Serial(ser,9600)
-#pay=[0,0,0]
-#cost=10
-#duration=[0,0,0]
-#timer=[0,0,0]
-prev_state=[1,1,1]
-parking_valid=[1,1,1]
-st_url="http://192.168.43.250:8080/update?p="
-#update=True
-while True:
+    global parking_valid,prev_state,req_need,st_url
+    while True:
+#        print(req_need)
+#        if req_need==False:
+            
+        line = str(serl.readline())
+        #        i=0
+        print(parking_valid)
+        parking_valid=[int(i) for i in line.split(",")]
+        if prev_state != parking_valid:
+            req_need=True
+            prev_state=parking_valid
+        time.sleep(2)
+def requ():
+    
+    global parking_valid,prev_state,req_need,st_url
+    
+    while True:
+#        print(req_need)
+#        print("hii")
+#        global parking_valid,prev_state
+        if req_need == True:
+            
+            st_url=st_url+str(parking_valid[0])+"&q="+str(parking_valid[1])+"&r="+str(parking_valid[2])
+            requests.get(url=st_url)
+            
+            req_need == False
+        time.sleep(6)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="com port")
+      # add expected arguments
+    parser.add_argument('--com', dest='com', required=True)
+
+      # parse args
+    comp= parser.parse_args()
+
+    ser = comp.com
+    serl = serial.Serial(ser,9600)
+    #pay=[0,0,0]
+    #cost=10
+    #duration=[0,0,0]
+    #timer=[0,0,0]
+    prev_state=[1,1,1]
+    parking_valid=[1,1,1]
+    st_url="http://192.168.43.250:8080/update?p="
+    req_need=False            
    
-
-    line = str(serl.readline())
-#        i=0
-    print(parking_valid)
+    t1 = threading.Thread(target=argu)
+    t2 = threading.Thread(target=requ)  
+    t1.setDaemon(True)
+    t2.setDaemon(False)
     
-    parking_valid=[int(i) for i in line.split(",")]
-    if prev_state != parking_valid:
-        st_url=st_url+str(parking_valid[0])+"&q="+str(parking_valid[1])+"&r="+str(parking_valid[2])
-        requests.get(url=st_url)
-        prev_state=parking_valid
-    
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
 #    prev_state=parking_valid
 #    for i in range(2):
 #        if parking_valid[i] == 0 and update==False:
